3f3aa87a00b82e6dc1c529048e978ae0d197dc6e
Lancelot <317286686@qq.com>
2018-12-05 16:57:33 +0800

    rollout mcts

--- mcts/rollout_mcts.py
+++ mcts/rollout_mcts.py
@@ -9,13 +9,17 @@ from ..utils.generic_utils import ProgBar
 from .base import MCTSBoard, Node
 
 class RolloutTraversal(threading.Thread):
-    def __init__(self, traversalQueue, updateQueue,
+    def __init__(self, traversalQueue,
+                 expandQueue, updateQueue,
                  root, get_virtual_value_function,
-                 c_puct=None, condition=None, start=True, **kwargs):
+                 depth=None, c_puct=None, condition=None,
+                 start=True, **kwargs):
         self.traversalQueue = traversalQueue
+        self.expandQueue = expandQueue
         self.updateQueue = updateQueue
         self.root = root
         self.get_virtual_value_function = get_virtual_value_function
+        self.depth = MCTS_TRAVERSAL_DEPTH if depth is None else depth
         self.c_puct = MCTS_C_PUCT if c_puct is None else c_puct
         self.condition = CONDITION if condition is None else condition
         super(RolloutTraversal, self).__init__(**kwargs)
@@ -25,9 +29,11 @@ class RolloutTraversal(threading.Thread):
 
     def run(self):
         traversalQueue = self.traversalQueue
+        expandQueue = self.expandQueue
         updateQueue = self.updateQueue
         root = self.root
         get_virtual_value_function = self.get_virtual_value_function
+        max_depth = self.depth
         c_puct = self.c_puct
         condition = self.condition
 
@@ -41,24 +47,32 @@ class RolloutTraversal(threading.Thread):
             condition.release()
 
             node = root
-            while True:
+            depth = 0
+            while depth < max_depth:
+                depth += 1
                 condition.acquire()
-                if not node.expanded:
-                    updateQueue.put_nowait((thread_index, board, node))
-                    condition.wait()
+                if node.value is not None:
                     condition.release()
                     break
+                if not node.expanded:
+                    expandQueue.put_nowait((board, node))
+                    condition.wait()
                 node = node.select(board, thread_index, virtual_loss,
                                    virtual_visit, c_puct)
                 condition.release()
+            condition.acquire()
+            updateQueue.put_nowait((thread_index, board, node))
+            condition.release()
 
 
 class RolloutMCTSBase(object):
-    def __init__(self, mixture_network,
-                 traverse_time, c_puct=None, thread_number=4,
-                 delete_threshold=10, condition=None):
-        self.mixture_network = mixture_network
+    def __init__(self, policy_network, rollout_network,
+                 traverse_time, depth=None, c_puct=None,
+                 thread_number=4, delete_threshold=10, condition=None):
+        self.policy_network = policy_network
+        self.rollout_network = rollout_network
         self.traverse_time = traverse_time
+        self.depth = MCTS_TRAVERSAL_DEPTH if depth is None else depth
         self.c_puct = MCTS_C_PUCT if c_puct is None else c_puct
         self.thread_number = thread_number
         self.delete_threshold = delete_threshold
@@ -70,27 +84,23 @@ class RolloutMCTSBase(object):
         self.condition = CONDITION if condition is None else condition
         self.threads = []
 
-    def traverse(self, board, start=True):
-        board = MCTSBoard(board, True)
+    def traverse(self, board, root, start=True):
         condition = self.condition
 
         traversalQueue = queue.Queue()
+        expandQueue = queue.Queue()
         updateQueue = queue.Queue()
         for index in range(self.traverse_time):
             traversalQueue.put_nowait((index, board.copy()))
 
-        Node(board.zobristKey, self.node_pool, self.left_pool,
-             self.delete_threshold, self.tuple_table, self.action_table,
-             self.value_table)
-        root = self.node_pool[board.zobristKey]
-        root.estimate(board)
-
         for _ in range(self.thread_number):
-            thread = RolloutTraversal(traversalQueue, updateQueue, root,
-                                         self.get_virtual_value_function, self.c_puct,
-                                         self.condition, start)
+            thread = RolloutTraversal(traversalQueue, expandQueue,
+                                      updateQueue, root,
+                                      self.get_virtual_value_function,
+                                      self.depth, self.c_puct,
+                                      self.condition, start)
             self.threads.append(thread)
-        return root, updateQueue
+        return expandQueue, updateQueue
 
     def update_nowait(self, tuples, progbar=None):
         left_tuples = []
@@ -105,32 +115,64 @@ class RolloutMCTSBase(object):
             left_tuples.append((index, board, node))
         return left_tuples
 
-    def expand_and_update(self, tuples, progbar=None):
-        indice = []
+    def expand(self, pairs):
         boards = []
         nodes = []
-        for index, board, node in tuples:
-            indice.append(index)
+        for board, node in pairs:
             boards.append(board)
             nodes.append(node)
-        distributions, values = self.mixture_network.predict_pairs(boards, sample=False)
+        distributions = self.policy_network.predict_actions(boards, sample=False)
         distributions = tolist(distributions)
-        values = tolist(values)
 
-        for idx, node in enumerate(nodes):
+        for board, distribution, node in zip(boards, distributions, nodes):
             if not node.expanded:
-                node.develop(boards[idx], distributions[idx])
-            index = indice[idx]
+                node.develop(board, distribution)
+
+    def update_by_rollout(self, tuples, progbar=None):
+        rollout_network = self.rollout_network
+        boards = [board for index, board, node in tuples]
+        players = [board.player for board in boards]
+        rollout_boards = set(boards)
+        while len(rollout_boards) > 0:
+            predict_indice = []
+            predict_boards = []
+            for idx, board in enumerate(boards):
+                if not board.is_over:
+                    predict_indice.append(idx)
+                    predict_boards.append(board)
+            actions = tolist(rollout_network.predict_actions(predict_boards))
+            for idx, action in zip(predict_indice, actions):
+                board = boards[idx]
+                board.move(action)
+                if board.is_over:
+                    rollout_boards.remove(boardx)
+        for idx, (index, board, node) in enumerate(tuples):
             virtual_loss, virtual_visit = self.get_virtual_value_function(index)
-            node.update(-values[idx], index, virtual_loss, virtual_visit)
+            if board.winner == DRAW:
+                value = 0.0
+            else:
+                player = players[idx]
+                value = -1.0 if board.winner == player else 1.0
+            node.update(value, index, virtual_loss, virtual_visit)
             if progbar is not None:
-                progbar.update()
+                probar.update()
 
     def mcts(self, board, verbose=1):
+        board = MCTSBoard(board, True)
         traverse_time = self.traverse_time
         node_pool = self.node_pool
         left_pool = self.left_pool
         condition = self.condition
+
+        Node(board.zobristKey, node_pool, left_pool,
+             self.delete_threshold, self.tuple_table, self.action_table,
+             self.value_table)
+        root = self.node_pool[board.zobristKey]
+        root.estimate(board)
+
+        if self.value_table[board.zobristKey] is not None:
+            actions = self.action_table[board.zobristKey]
+            return actions[np.random.randint(len(actions))]
 
         if verbose:
             progbar = ProgBar(traverse_time)
@@ -138,10 +180,15 @@ class RolloutMCTSBase(object):
         else:
             progbar = None
         total_count = 0
-        root, updateQueue = self.traverse(board, True)
+        expandQueue, updateQueue = self.traverse(board, root, True)
         while True:
-            tuples = []
+            pairs = []
             condition.acquire()
+            while not expandQueue.empty():
+                pairs.append(expandQueue.get_nowait())
+            if len(pairs) > 0:
+                self.expand(pairs)
+            tuples = []
             while not updateQueue.empty():
                 tuples.append(updateQueue.get_nowait())
             if len(tuples) == 0:
@@ -158,7 +205,7 @@ class RolloutMCTSBase(object):
                 condition.release()
                 continue
 
-            self.expand_and_update(left_tuples, progbar)
+            self.update_by_rollout(left_tuples, progbar)
             total_count += len(left_tuples)
             condition.notifyAll()
             condition.release()
@@ -196,6 +243,7 @@ class RolloutMCTSBase(object):
     def get_config(self):
         config = {
             'traverse_time': self.traverse_time,
+            'depth': self.depth,
             'c_puct': self.c_puct,
             'thread_time': self.thread_number,
             'delete_threshold': self.delete_threshold,
@@ -213,10 +261,11 @@ class RolloutMCTSBase(object):
         return config
 
     @classmethod
-    def from_config(cls, config, mixture_network):
+    def from_config(cls, config, policy_network, rollout_network):
         tree = cls(
-            mixture_network,
+            policy_network, rollout_network,
             traverse_time=config['traverse_time'],
+            depth=config['depth'], 
             c_puct=config['c_puct'],
             thread_number=config['thread_number'],
             delete_threshold=config['delete_threshold'],
