d81a48cd447812e73a0245f5e4a63b6056269c1c
Lancelot <317286686@qq.com>
2018-12-05 19:18:41 +0800

    mcts evaluation

--- mcts/test/test_evaluation_mcts.py
+++ mcts/test/test_evaluation_mcts.py
@@ -2,12 +2,24 @@ from AlphaGomoku.mcts.evaluation_mcts import EvaluationMCTSBase
 from AlphaGomoku.neural_networks import get_network
 from AlphaGomoku.neural_networks.keras.weights import get_weight_file
 from AlphaGomoku.board import Board
+from AlphaGomoku.play import Human, Game
 
 mixture = get_network('mixture', 'resnet', 'keras', stack_nb=1)
 mixture.load_weights(get_weight_file('pre', mixture.network.name, 0))
 mcts = EvaluationMCTSBase(mixture, 500, thread_number=4, delete_threshold=10)
 
-board = Board(visualization=True)
-while not board.is_over:
-    action = mcts.mcts(board, 1)
-    board.move(action)
+mcts.get_action = lambda board: mcts.mcts(board, 1)
+
+pattern = int(input('1: player vs ai, 2: ai vs player, 3: ai vs ai: '))
+if pattern == 1:
+    black = Human()
+    white = mcts
+elif pattern == 2:
+    black = mcts
+    white = Human()
+else:
+    black = mcts
+    white = mcts
+
+game = Game(black, white, visualization=True, visualization_time=3)
+game.play()
--- mcts/test/test_rollout_mcts.py
+++ mcts/test/test_rollout_mcts.py
@@ -2,13 +2,25 @@ from AlphaGomoku.mcts.rollout_mcts import RolloutMCTSBase
 from AlphaGomoku.neural_networks import get_network
 from AlphaGomoku.neural_networks.keras.weights import get_weight_file
 from AlphaGomoku.board import Board
+from AlphaGomoku.play import Human, Game
 
 policy = get_network('policy', 'resnet', 'keras', stack_nb=1)
 # policy.load_weights(get_weight_file('pre', policy.network.name, 0))
 mcts = RolloutMCTSBase(policy, policy, 100, thread_number=4,
                        depth=6, delete_threshold=10)
 
-board = Board(visualization=True)
-while not board.is_over:
-    action = mcts.mcts(board, 1)
-    board.move(action)
+mcts.get_action = lambda board: mcts.mcts(board, 1)
+
+pattern = int(input('1: player vs ai, 2: ai vs player, 3: ai vs ai: '))
+if pattern == 1:
+    black = Human()
+    white = mcts
+elif pattern == 2:
+    black = mcts
+    white = Human()
+else:
+    black = mcts
+    white = mcts
+
+game = Game(black, white, visualization=True, visualization_time=3)
+game.play()
