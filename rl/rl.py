3a02411215d3d8bc7843bb0e0c8a62cd8cd47eb9
Lancelot <317286686@qq.com>
2018-12-12 17:20:25 +0800

    rl

--- rl/rl.py
+++ rl/rl.py
@@ -5,13 +5,14 @@ from ..common import *
 from ..board import Board
 from ..mcts import get as mcts_get
 from ..neural_networks.keras import get as network_get
+from ..neural_networks.weights import get_config_file, get_weight_file
 from ..utils.json_utils import json_dump_tuple, json_load_tuple
 
 
 rng = np.random
 
 
-class SelfPlay(object):
+class SelfPlayBase(object):
     def __init__(self, mcts, number=1000, batch_size=10, initialize=True):
         self.mcts = mcts
         self.number = number
@@ -21,113 +22,186 @@ class SelfPlay(object):
             self.index = index
             self.boards = set(Board() for _ in range(index))
             self.step = 0
+            self.finished = 0
 
-    def run(self, cache_step=None, cache_folder=''):
+    def generator(self):
         mcts = self.mcts
         number = self.number
         batch_size = self.batch_size
-        index = self.index
         boards = self.boards
-        if cache_step is not None:
-            if isinstance(mcts, mcts_get('EvaluationMCTSBase')):
-                config_path, weight_path = self.get_mixture_network_path(folder)
-                json_dump_tuple(mcts.mixture_network.get_config(), config_path)
-                mcts.mixture_network.save_weights(weight_path)
-            else:
-                policy_config_path, policy_weight_path = self.get_policy_network_path(folder)
-                json_dump_tuple(mcts.policy_network.get_config(), policy_config_path)
-                mcts.policy.save_weights(policy_weight_path)
-                rollout_config_path, rollout_weight_path = self.get_rollout_network_path(folder)
-                json_dump_tuple(mcts.rollout_network.get_config(), rollout_config_path)
-                mcts.rollout.save_weights(rollout_weight_path)
-            config_path = self.get_config_path(folder)
         while len(boards) > 0:
+            yield self.step, self.finished
             temp_boards = list(boards)
             actions = tolist(mcts.mcts(temp_boards, verbose=0))
             for board, action in zip(temp_boards, actions):
                 board.move(action)
                 if board.is_over:
                     boards.remove(board)
-                    if index < number:
+                    self.finished += 1
+                    if self.index < number:
                         boards.add(Board())
-                        index += 1
+                        self.index += 1
             self.step += 1
-            if cache_step is not None and self.step % cache_step == 0:
-                json_dump_tuple(self.get_config(), config_path)
+        self.raw_samples = self.get_samples_from_mcts(mcts)
+        yield self.step, self.finished
 
-    @classmethod
-    def get_mixture_network_path(cls, folder):
-        config_path = os.path.join(folder, 'mixture_network_config.json')
-        weight_path = os.path.join(folder, 'mixture_network_weights.npz')
-        return config_path, weight_path
+    def get_samples_from_mcts(self):
+        raise NotImplementedError('`get_samples_from_mcts` has not '
+                                  'been implemented')
 
-    @classmethod
-    def get_policy_network_path(cls, folder):
-        policy_config_path = os.path.join(folder, 'policy_network_config.json')
-        policy_weight_path = os.path.join(folder, 'policy_network_weights.npz')
-        return policy_config_path, policy_weight_path
+    def get_tensor_from_history(self, history):
+        tensor = np.zeros((BOARD_SIZE, BOARD_SIZE, 3), dtype=FLOATX) \
+                 +np.array([[[0, 0, 1]]])
+        player = BLACK if len(history) % 2 == 0 else WHITE
+        color = BLACK
+        for action in history:
+            if color == player:
+                tensor[action+(0, )] = 1.0
+            else:
+                tensor[action+(1, )] = 1.0
+            tensor[action+(2, )] = 0.0
+            color = player_map(color)
+        return tensor
 
-    @classmethod
-    def get_rollout_network_path(cls, folder):
-        rollout_config_path = os.path.join(folder, 'rollout_network_config.json')
-        rollout_weight_path = os.path.join(folder, 'rollout_network_weights.npz')
-        return rollout_config_path, rollout_weight_path
+    def get_tensor_from_visits(self, visits, get_max):
+        tensor = np.zeros((BOARD_SIZE, BOARD_SIZE), dtype=FLOATX)
+        if get_max:
+            max_action = None
+            max_visit = 0
+            for action, visit in visits:
+                if visit > max_visit:
+                    max_action = action
+                    max_visit = visit
+            tensor[max_action] = 1.0
+        else:
+            s = float(sum([visit for _, visit in visits]))
+            for action, visit in visits:
+                tensor[action] = visit / s
+        return tensor
 
-    @classmethod
-    def get_config_path(cls, folder):
-        return os.path.join(folder, 'self_play.json')
+    def get_samples(self):
+        if not hasattr(self, 'raw_samples'):
+            raise Exception('Self-play has not been finished')
+        return self.process_raw_samples(self.raw_samples)
+
+    def process_raw_samples(self, raw_samples):
+        raise NotImplemented('`process_raw_samples` has not been implemented')
 
     def get_config(self):
         config = [
-            'mcts_cls_name': self.mcts.__class__.__name__,
-            'mcts_config': self.mcts.get_config(),
+            'mcts': {
+                'class_name': self.mcts.__class__.__name__,
+                'config': self.mcts.get_config()
+            },
             'number': self.number,
             'batch_size': self.batch_size,
             'index': self.index,
-            'step': self.step
+            'step': self.step,
+            'finished': self.finished
         ]
-        if isinstance(self.mcts, mcts_get('EvaluationMCTSBase')):
-            config['network_cls'] = [self.mcts.mixture_network.__class__.__name__]
-        else:
-            config['network_cls'] = [
-                self.mcts.policy_network.__class__.__name__,
-                self.mcts.rollout_network.__class__.__name__
-            ]
+        if hasattr(self, 'raw_samples'):
+            config['raw_samples'] = tuples_to_json(self.raw_samples)
         return config
 
     @classmethod
     def from_config(cls, config):
-        mcts_cls = mcts_get(config['mcts_cls_name'])
-        mcts_config = config['mcts_config']
-        if issubclass(mcts_cls, mcts_get('EvaluationMCTSBase')):
-            config_path, weight_path = cls.get_mixture_network_path(folder)
-            mixture_network = network_get(config['network_cls'][0]).load_config(config_path)
-            mixture_network.load_weights(weight_path)
-            networks = {'mixture_network': mixture_network}
-        else:
-            policy_config_path, policy_weight_path = cls.get_policy_network_path(folder)
-            policy_network = network_get(config['network_cls'][0]).load_config(policy_config_path)
-            policy_network.load_weights(policy_weight_path)
-            rollout_config_path, rollout_weight_path = cls.get_rollout_network_path(folder)
-            rollout_network = network_get(config['network_cls'][1]).load_config(rollout_config_path)
-            rollout_network.load_weights(rollout_weight_path)
-            networks = {'policy_network': policy_network, 'rollout_network': rollout_network}
-        mcts, boards = mcts_cls.from_config(mcts_config, **networks)
-        sf = cls(mcts, config['number'], config['batch_size'], False)
-        sf.index = config['index']
-        sf.boards = boards
-        sf.step = config['step']
+        mcts, boards = mcts_get(config.pop('mcts'))
+        raw_samples = config.pop('raw_samples', None)
+        sf = cls(mcts, initialize=False)
+        sf.__dict__.update(config)
+        sf.boards = set(board for board in boards if not board.is_over)
+        if raw_samples is not None:
+            sf.raw_samples = raw_samples
         return sf
 
 
-def evaluator():
-    pass
+class EvaluationSelfPlay(SelfPlayBase):
+    def get_samples_from_mcts(self, mcts):
+        tuples = []
+        player_dict = {0: BLACK, 1: WHITE}
+        for board, index in mcts.board_indice.items():
+            winner = board.winner
+            for history, visits in mcts.visit_container:
+                player = player_dict[len(history) % 2]
+                if winner == DRAW:
+                    value = MCTS_DRAW_VALUE
+                elif winner == player:
+                    value = MCTS_WIN_VALUE
+                else:
+                    value = MCTS_LOSS_VALUE
+                tuples.append((history, visits, value))
+        return tuples
 
-def generator():
-    pass
+    def process_raw_samples(self, raw_samples):
+        board_tensors = []
+        action_tensors = []
+        values = []
+        for history, visits, value in raw_samples:
+            board_tensor.append(self.get_tensor_from_history(history))
+            action_tensors.append(
+                self.get_tensor_from_visits(visits, len(history)>=MCTS_RL_SAMPLE_STEP)
+            )
+            values.append(value)
+        return board_tensors, action_tensors, values
 
-def trainer():
-    pass
+    def get_config(self, index):
+        config = super(EvaluationSelfPlay, self).get_config()
+        mixture = self.mcts.mixture
+        config['mixture'] = {
+            'class_name': mixture.__class__.__name__,
+            'config': mixture.get_config(True, 'rl', index)
+        }
+        return config
+
+    @classmethod
+    def from_config(cls, config):
+        mixture = network_get(config.pop('mixture'))
+        sf = super(EvaluationSelfPlay, cls).from_config(config)
+        sf.mcts.mixture = mixture
+        return sf
+
+
+class Evaluator(object):
+    def __init__(self, best_mcts, new_mcts, boards_each_side):
+        self.mcts = [best_mcts, new_mcts]
+        self.boards_each_side = boards_each_side
+
+    def generator(self):
+        mcts = self.mcts
+        boards = set()
+        for ply in [0, 1]:
+            for _ in range(self.boards_each_side):
+                boards.add((board, ply))
+        step = 0
+        finished = 0
+        player = 0
+        win = 0
+        loss = 0
+        draw = 0
+        while len(boards) > 0:
+            yield step, finished, win, loss, draw
+            temp_boards = [board for board, ply in boards if ply == player]
+            actions = tolist(mcts[player].mcts(temp_boards))
+            for board, action in zip(temp_boards, actions)
+                board.move(action)
+                boards.remove((board, player))
+                if board.is_over:
+                    finished += 1
+                    if board.winner == DRAW:
+                        draw += 1
+                    elif player == 1:
+                        win += 1
+                    else:
+                        loss += 1
+                else:
+                    boards.add((board, player ^ 1))
+            player ^= 1
+        step += 1
+        yield step, finished, win, loss, draw
+
+
+class TrainerGenerator(object):
+    def __init__(self, )
 
 def main_loop():
     pass
